4ade4a71fa030c2408524233e85756399a9e0298
Vriur <nathgh04@gmail.com>
2025-01-28 23:58:36 -0600

    (feat) Pokemon's card images and styling

--- src/app/types/[typeName]/page.tsx
+++ src/app/types/[typeName]/page.tsx
@@ -6,6 +6,11 @@ interface Pokemon {
     slot: number;
 }
 
+/* This arrays are need it because Tailwind dump every class that wasn't hardcoded on the code. */
+const typeBorderColours = ['border-normal', 'border-fire', 'border-water', 'border-electric', 'border-grass', 'border-ice', 'border-fighting', 'border-poison', 'border-ground', 'border-flying', 'border-psychic', 'border-bug', 'border-rock', 'border-ghost', 'border-dragon', 'border-dark', 'border-steel', 'border-fairy', 'border-stellar', 'border-unknown'];
+const typeBgColours = ['bg-normal', 'bg-fire', 'bg-water', 'bg-electric', 'bg-grass', 'bg-ice', 'bg-fighting', 'bg-poison', 'bg-ground', 'bg-flying', 'bg-psychic', 'bg-bug', 'bg-rock', 'bg-ghost', 'bg-dragon', 'bg-dark', 'bg-steel', 'bg-fairy', 'bg-stellar', 'bg-unknown'];
+const typeTextColours = ['text-normal', 'text-fire', 'text-water', 'text-electric', 'text-grass', 'text-ice', 'text-fighting', 'text-poison', 'text-ground', 'text-flying', 'text-psychic', 'text-bug', 'text-rock', 'text-ghost', 'text-dragon', 'text-dark', 'text-steel', 'text-fairy', 'text-stellar', 'text-unknown'];
+
 export default async function TypeName ( 
     { params }: { params: Promise<{ typeName: string }> } 
 ){
@@ -17,15 +22,22 @@ export default async function TypeName (
 
     return (
         <div className='flex justify-center content-center h-screen'>
-            <div className='grid grid-cols-5 gap-4 p-10'>
+            <div className='grid md:grid-cols-5 grid-cols-2 gap-4 p-10'>
                 {
-                    pokemon.pokemon.map((element: Pokemon) => {
-                        console.log(element.slot);
-                        console.log(element.pokemon.url);
+                    pokemon.pokemon.map((pokemon: Pokemon) => {
+                        /* Extract the identificator of to pokemon to get the corresponding image. That link is one of the repos where the images are stored of PokeAPI. */
+                        let pokemonIdentifier = pokemon.pokemon.url.match(/\d+/g);
+                        let pokemonImageURL = "";
+
+                        /* Concatenate the URL of the corresponding image */
+                        if(pokemonIdentifier){
+                            pokemonImageURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIdentifier[1]}.png` 
+                        }
+                        
                         return (
-                            <div className='italic font-sans font-semibold text-xl capitalize justify-center content-center text-center p-8 border-2 rounded-lg transition delay-200 duration-300 ease-in-out text-palette-primary border-palette-primary hover:text-palette-white hover:bg-palette-primary'>
-                                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10149.png"></img>
-                                <label>{element.pokemon.name}</label>
+                            <div key={pokemon.pokemon.name} className={`group justify-center content-center text-center p-8 border-4 rounded-lg transition delay-200 duration-300 ease-in-out text-${typeName} border-${typeName} bg-${typeName} hover:bg-opacity-50`}>
+                                <img src={pokemonImageURL} className="justify-center content-center"></img>
+                                <label className='group-hover:text-palette-black italic font-sans font-semibold md:text-xl text-sm text-center capitalize'>{pokemon.pokemon.name}</label>
                             </div>
                         )
                     })
--- src/app/types/page.tsx
+++ src/app/types/page.tsx
@@ -5,21 +5,22 @@ interface Type {
     url: string;
 }
 
+/* This arrays are need it because Tailwind dump every class that wasn't hardcoded on the code. */
+const typeBorderColours = ['border-normal', 'border-fire', 'border-water', 'border-electric', 'border-grass', 'border-ice', 'border-fighting', 'border-poison', 'border-ground', 'border-flying', 'border-psychic', 'border-bug', 'border-rock', 'border-ghost', 'border-dragon', 'border-dark', 'border-steel', 'border-fairy', 'border-stellar', 'border-unknown'];
+const typeBgColours = ['bg-normal', 'bg-fire', 'bg-water', 'bg-electric', 'bg-grass', 'bg-ice', 'bg-fighting', 'bg-poison', 'bg-ground', 'bg-flying', 'bg-psychic', 'bg-bug', 'bg-rock', 'bg-ghost', 'bg-dragon', 'bg-dark', 'bg-steel', 'bg-fairy', 'bg-stellar', 'bg-unknown'];
+
 export default async function Types() {
     const data = await fetch("https://pokeapi.co/api/v2/type")
     const types = await data.json();
-    const typeTextColours = ['text-normal', 'text-fire', 'text-water', 'text-electric', 'text-grass', 'text-ice', 'text-fighting', 'text-poison', 'text-ground', 'text-flying', 'text-psychic', 'text-bug', 'text-rock', 'text-ghost', 'text-dragon', 'text-dark', 'text-steel', 'text-fairy', 'text-stellar', 'text-unknown']; 
-    const typeBorderColours = ['border-normal', 'border-fire', 'border-water', 'border-electric', 'border-grass', 'border-ice', 'border-fighting', 'border-poison', 'border-ground', 'border-flying', 'border-psychic', 'border-bug', 'border-rock', 'border-ghost', 'border-dragon', 'border-dark', 'border-steel', 'border-fairy', 'border-stellar', 'border-unknown'];
-    const typeBgColours = ['bg-normal', 'bg-fire', 'bg-water', 'bg-electric', 'bg-grass', 'bg-ice', 'bg-fighting', 'bg-poison', 'bg-ground', 'bg-flying', 'bg-psychic', 'bg-bug', 'bg-rock', 'bg-ghost', 'bg-dragon', 'bg-dark', 'bg-steel', 'bg-fairy', 'bg-stellar', 'bg-unknown'];
 
     return (
-        <div className='flex justify-center content-center h-screen'>
-            <div className='grid grid-cols-5 gap-4 p-10'>
+        <div className='flex justify-center content-center h-screen w-screen'>
+            <div className='grid grid-cols-3 md:grid-cols-5 gap-4 p-4 lg:p-10'>
                 {
                     types.results.map((type: Type) => {
                         return (
-                            <Link key={type.name} href={`/types/${type.name}`} className={`justify-center content-center text-center p-8 border-4 rounded-lg transition delay-200 duration-300 ease-in-out text-palette-black border-${type.name} bg-${type.name} hover:text-palette-black hover:bg-palette-grey`}>
-                                <label className='italic font-sans font-semibold text-xl  text-center capitalize'>{type.name}</label>   
+                            <Link key={type.name} href={`/types/${type.name}`} className={`justify-center content-center text-center lg:p-8 border-4 rounded-lg transition delay-200 duration-300 ease-in-out text-palette-black border-${type.name} bg-${type.name} hover:bg-opacity-50`}>
+                                <label className='italic font-sans font-semibold text-xl text-center capitalize'>{type.name}</label>   
                             </Link>
                         )
                     })
